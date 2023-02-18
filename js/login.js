0e890a05eba195d89215361fde0918156500e83e
“Harry” <boom0525300@gmail.com>
2023-02-19 00:24:06 +0800

    loginPage basic enviroment

---
+++ js/login.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.mobile-menuBTN').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('change');
+        $('.navbar').toggleClass('show');
+    });
+    
+});
